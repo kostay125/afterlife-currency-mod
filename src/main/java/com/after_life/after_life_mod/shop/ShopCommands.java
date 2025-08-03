e36c649f7903f80363a72de6333f0318dfd46876
ivkin <ivkin1401@yandex.ru>
2025-08-03 04:30:16 +0300

    commit

--- src/main/java/com/after_life/after_life_mod/shop/ShopCommands.java
+++ src/main/java/com/after_life/after_life_mod/shop/ShopCommands.java
@@ -1,6 +1,7 @@
 package com.after_life.after_life_mod.shop;
 
 import com.mojang.brigadier.CommandDispatcher;
+import com.mojang.logging.LogUtils;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.Commands;
 import net.minecraft.network.chat.Component;
@@ -8,9 +9,14 @@ import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.SimpleMenuProvider;
 import com.after_life.after_life_mod.shop.gui.ShopMenu;
 import com.after_life.after_life_mod.shop.gui.ShopMenuType;
+import net.minecraft.world.SimpleMenuProvider;
+import org.slf4j.Logger;
+
 
 public class ShopCommands {
+    private static final Logger LOGGER = LogUtils.getLogger();
     public static void register(CommandDispatcher<CommandSourceStack> dispatcher) {
+        LOGGER.info("Регистрация команд магазина"); // Добавьте это
         dispatcher.register(
                 Commands.literal("shop")
                         .executes(ctx -> {
